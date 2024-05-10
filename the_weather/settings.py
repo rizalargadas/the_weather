43f07d95ac035c8f85a14035e06f68b5529ee09d
Riza May Largadas <138735214+rizalargadas@users.noreply.github.com>
2024-05-10 10:13:47 +0800

    Update settings.py for railway
    added allowed hosts
    csrf trusted origins

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-5fc4.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5fc4.up.railway.app/']
 
 
 # Application definition
